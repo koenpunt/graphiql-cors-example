081557d53a41c2fbb93369730c69f604027d2ab1
Koen Punt <koen@koenpunt.nl>
2023-03-08 15:57:12 +0100

    initial spring application with minimal schema

---
+++ src/main/kotlin/com/example/graphiqlcorsexample/GraphiqlCorsExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.graphiqlcorsexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphiqlCorsExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<GraphiqlCorsExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/graphiqlcorsexample/GraphiqlCorsExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.graphiqlcorsexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphiqlCorsExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
